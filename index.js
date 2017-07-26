7103c88c23ce95f2b548d51ccbde087a4fe064d9
zaknight <letrunghoa90@gmail.com>
2017-07-26 18:04:50 +0700

    check button

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Dom from 'react-dom';
-import Button from './src/Button/Button';
+// import React from 'react';
+// import Dom from 'react-dom';
+// import Button from './src/Button/Button';
 
-Dom.render(
-    <div>
-        <Button raised dense ripplePrimary>Co ripple nha</Button>
-        <Button raised dense>Khong Ripple day nha</Button>
-    </div>
-    ,
-    document.querySelector('#root')
-);
+// Dom.render(
+//     <div>
+//         <Button raised dense ripplePrimary>Co ripple nha</Button>
+//         <Button raised dense>Khong Ripple day nha</Button>
+//     </div>
+//     ,
+//     document.querySelector('#root')
+// );
+
+export {default as Button} from './src/Button';
